6fafbe8e28f5e37b6b4fba75e408727b422358b9
unknown <alexisdongmariano@gmail.com>
2020-10-30 05:03:51 +0800

    added Python-02 Basic Data Types-01 Lists.py

---
+++ Python/02 - Basic Data Types/01 - Lists.py
@@ -0,0 +1,12 @@
+# ==============================
+#         Information
+# ==============================
+
+# Link: https://www.hackerrank.com/challenges/python-lists/problem
+# Difficulty: Easy
+# Max Score: 10
+# Language: Python
+
+# ==============================
+#         Solution
+# ==============================
