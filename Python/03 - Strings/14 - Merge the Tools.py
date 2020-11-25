90977ee53e012c97a90f64c5078d66ecd00cf402
unknown <alexisdongmariano@gmail.com>
2020-11-26 03:15:35 +0800

    14 - Merge the Tools.py

---
+++ Python/03 - Strings/14 - Merge the Tools.py
@@ -0,0 +1,34 @@
+# ==============================
+#         Information
+# ==============================
+
+# Title: 13 - The Minion Game
+# Link: https://www.hackerrank.com/challenges/merge-the-tools/problem
+# Difficulty: Medium
+# Max Score: 40
+# Language: Python
+
+# ==============================
+#         Solution
+# ==============================
+
+
+def merge_the_tools(string, k):
+    # your code goes here
+    sub_str = []
+    output_str = ''
+
+    for index, letter in enumerate(string, 1):
+        if letter not in sub_str:
+            sub_str.append(letter)
+
+        if index % k == 0:
+            output_str += ''.join(sub_str) + '\n'
+            sub_str = []
+
+    print(output_str)
+
+
+if __name__ == '__main__':
+    string, k = raw_input(), int(raw_input())
+    merge_the_tools(string, k)
